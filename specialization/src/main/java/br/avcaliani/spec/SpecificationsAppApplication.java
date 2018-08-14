8ab27df461eb2fe86fb66169f23ccaa9ad534a81
Anthony Caliani <avcaliani.it@gmail.com>
2018-08-14 20:05:32 -0300

    Adding new project. I haven't finished it yet.

---
+++ specialization/src/main/java/br/avcaliani/spec/SpecificationsAppApplication.java
@@ -0,0 +1,12 @@
+package br.avcaliani.spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationsAppApplication.class, args);
+	}
+}
---
+++ specialization/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(id, name, passport, email) values(1, 'Anthony', 'A1234567', 'anthony@github.com');
+insert into student(id, name, passport, email) values(2, 'Joe', 'J1234567', 'joe@github.com');
+insert into student(id, name, passport, email) values(3, 'Diana', 'D1234567', 'diana@github.com');
+insert into student(id, name, passport, email) values(4, 'Rachel', 'R1234567', 'rachel@github.com');
