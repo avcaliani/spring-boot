9c415e7956a76a78fcb4715ddb849c5831971736
Anthony Vilarim Caliani <avcaliani.it@gmail.com>
2018-08-15 14:29:32 -0300

    Updating some code.

--- specialization/src/main/java/br/avcaliani/spec/controllers/StudentController.java
+++ specialization/src/main/java/br/avcaliani/spec/controllers/StudentController.java
@@ -2,14 +2,12 @@ package br.avcaliani.spec.controllers;
 
 import br.avcaliani.spec.entities.Student;
 import br.avcaliani.spec.services.StudentService;
-// import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@RestController()
+@RestController
 @RequestMapping("/student")
 @CrossOrigin(allowedHeaders = "*")
 public class StudentController {
@@ -18,8 +16,7 @@ public class StudentController {
     private StudentService service;
 
     @GetMapping
-    public List<Student> find(HttpServletRequest request) {
-        System.out.println("HttpServletRequest -> " + request.toString());
+    public List<Student> find() {
         return this.service.findAll();
     }
 
