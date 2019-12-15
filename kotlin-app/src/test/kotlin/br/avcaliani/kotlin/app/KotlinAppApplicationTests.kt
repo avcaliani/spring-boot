f99f41277a6a49447fac5e35abb96984016c4ea6
Anthony <avcaliani.it@gmail.com>
2019-12-15 20:03:43 -0300

    Initial commit

---
+++ kotlin-app/src/test/kotlin/br/avcaliani/kotlin/app/KotlinAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.avcaliani.kotlin.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
