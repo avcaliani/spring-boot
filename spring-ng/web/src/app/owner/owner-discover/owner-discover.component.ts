ab8f83cd82dcda7acdd6f2f6ffd6f8690fabfca3
Anthony <avcaliani.it@gmail.com>
2019-12-22 22:54:58 -0300

    Removing project

--- spring-ng/web/src/app/owner/owner-discover/owner-discover.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-owner-discover',
-  template: `<div><a [routerLink]="[ '/contact' ]">...</a></div>`,
-  styleUrls: ['./owner-discover.component.scss']
-})
-export class OwnerDiscoverComponent { }
--- spring-ng/web/src/app/owner/owner.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Contact {
-  name: string;
-  tag: string;
-  github: string;
-  gitlab: string;
-}
